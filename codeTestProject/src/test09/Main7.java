6435025afd91b307a9fa4cbe6eb760765c8b78f4
caramelpopcornKHU <hyungmin8528@naver.com>
2026-02-03 18:20:09 +0900

    union find prac

---
+++ codeTestProject/src/test09/Main7.java
@@ -0,0 +1,62 @@
+package test09;
+
+import java.io.*;
+import java.util.*;
+
+public class Main7 {
+
+	static int[] unf;
+
+	public static int Find(int v) {
+		if (v == unf[v])
+			return v;
+		else {
+			return unf[v] = Find(unf[v]);
+		}
+	}
+
+	public static void Union(int a, int b) {
+		int fa = Find(a);
+		int fb = Find(b);
+		if (fa != fb) {
+			unf[fa] = fb;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		unf = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			unf[i] = i;
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			Union(a, b);
+		}
+
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		int fa = Find(a);
+		int fb = Find(b);
+
+		if (fa == fb) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+
+	}
+
+}
