5c0fd7e50bddfeb1a9b08e69ca6647d749b4cffa
caramelpopcornKHU <hyungmin8528@naver.com>
2026-02-10 09:40:01 +0900

    DP LIS - 다시 볼 문제

---
+++ codeTestProject/src/test10/Main3.java
@@ -0,0 +1,46 @@
+package test10;
+
+import java.io.*;
+import java.util.*;
+
+public class Main3 {
+
+	public int[] dy;
+
+	public int solution(int n, int[] arr) {
+		int answer = 0;
+
+		dy = new int[n];
+		dy[0] = 1;
+		for (int i = 1; i < n; i++) {
+			int max = 0;
+			for (int j = i - 1; j >= 0; j--) {
+				if (arr[j] < arr[i] && dy[j] > max)
+					max = dy[j];
+			}
+
+			dy[i] = max + 1;
+			answer = Math.max(answer, dy[i]);
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Main3 T = new Main3();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(T.solution(n, arr));
+
+	}
+
+}
